d3be42b3b0ae734c5120811b7608f2c818c59f9c
dave-santiano <dss441@nyu.edu>
2019-06-19 18:21:35 +0800

    temporarily removed some stuff

---
+++ works/Untitled-1.js
@@ -0,0 +1,17 @@
+var tempX;
+var tempY;
+
+function draw(){
+    
+
+
+    //calculations
+    x = someValue;
+    y = someValue;
+    var vector = (x, y);
+    //vector calculation
+
+
+    tempX = calculatedXValue;
+    tempY = calculatedYValue;
+}
